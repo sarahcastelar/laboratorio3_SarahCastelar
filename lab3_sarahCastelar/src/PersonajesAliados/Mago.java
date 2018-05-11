b1ef26a0486352cda5b8cdcef180b7b866cf1722
sarahcastelar <sarahgcastelar@gmail.com>
2018-05-11 13:48:32 -0600

    Cree la clase personaje para luego hacer la herencia a los demas personajes.

---
+++ lab3_sarahCastelar/src/PersonajesAliados/Mago.java
@@ -0,0 +1,32 @@
+
+package PersonajesAliados;
+
+public class Mago {
+    
+    private int hp = 120; 
+    private int ap = 50;
+    private int ad = 20;
+    private int eficacia = 70;
+    private int probCritico = 2;
+    private int suerte = 20;
+    private int velocidad = 10; 
+    private String gritoBatalla = "gsssss";
+    private int edad;
+    private String nombre;
+    private int estatura;
+    private int peso;
+    private String colorPiel;
+    private String colorPelo;
+    
+    public Mago() {
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ lab3_sarahCastelar/src/lab3_sarahcastelar/Lab3_sarahCastelar.java
@@ -0,0 +1,26 @@
+
+package lab3_sarahcastelar;
+
+import javax.swing.JOptionPane;
+
+public class Lab3_sarahCastelar {
+
+    
+    public static void main(String[] args) {
+        int option = -1;
+        
+        while (option != 3){
+            option = Integer.parseInt(JOptionPane.showInputDialog("Ingrese opcion:  \n"
+                    + "1. Iniciar \n"
+                    + "2. Nivel de Dificultad\n "
+                    + "3. Salir"));
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
+}
